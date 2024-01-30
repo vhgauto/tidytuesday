d3b364c272e73b7928d544402c7a8519c07dcbc8
vhgauto <victor.gauto@outlook.com>
2024-01-30 11:21:17 -0300

    modifico las fuentes para lectura local

---
+++ plantilla.R
@@ -0,0 +1,70 @@
+
+# paquetes ----------------------------------------------------------------
+
+library(glue)
+library(ggtext)
+library(showtext)
+library(tidyverse)
+
+# fuente ------------------------------------------------------------------
+
+# colores
+c1 <- "#000000"
+c2 <- "#000000"
+c3 <- "#000000"
+
+# fuente: Ubuntu
+font_add(
+  family = "ubuntu", 
+  regular = "fuente/Ubuntu-Regular.ttf",
+  bold = "fuente/Ubuntu-Bold.ttf",
+  italic = "fuente/Ubuntu-Italic.ttf")
+
+# fuente: Victor
+font_add(
+  family = "victor", 
+  regular = "fuente/VictorMono-ExtraLight.ttf",
+  bold = "fuente/VictorMono-VariableFont_wght.ttf",
+  italic = "fuente/VictorMono-ExtraLightItalic.ttf")
+
+# íconos
+font_add("fa-brands", "icon/Font Awesome 6 Brands-Regular-400.otf")
+
+showtext_auto()
+showtext_opts(dpi = 300)
+
+# caption
+fuente <- glue(
+  "Datos: <span style='color:{c3};'><span style='font-family:mono;'>",
+  "{{<b>tidytuesdayR</b>}}</span> semana {semana_numero}. ",
+  "XXX fuente de datos XXX.</span>")
+autor <- glue("<span style='color:{c3};'>**Víctor Gauto**</span>")
+icon_twitter <- glue("<span style='font-family:fa-brands;'>&#xf099;</span>")
+icon_github <- glue("<span style='font-family:fa-brands;'>&#xf09b;</span>")
+icon_mastodon <- glue("<span style='font-family:fa-brands;'>&#xf4f6;</span>")
+usuario <- glue("<span style='color:{c3};'>**vhgauto**</span>")
+sep <- glue("**|**")
+
+mi_caption <- glue(
+  "{fuente}<br>{autor} {sep} {icon_github} {icon_twitter} {icon_mastodon} 
+  {usuario}")
+
+# datos -------------------------------------------------------------------
+
+tuesdata <- tidytuesdayR::tt_load(año, semana_numero)
+
+# figura ------------------------------------------------------------------
+
+# figura
+# g <- ggplot()
+
+# guardo
+# ggsave(
+#   plot = g,
+#   filename = "semana_carpeta/viz.png",
+#   width = 30,
+#   height = 30,
+#   units = "cm")
+
+# abro
+# browseURL("semana_carpeta/viz.png")
