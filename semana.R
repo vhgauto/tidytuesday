9a20cbac62d4f12c7b5bfa723fd2ba19939a37f8
vhgauto <victor.gauto@outlook.com>
2024-06-18 19:46:50 -0300

    remuevo el llamado a los paquetes y limpio el environment de variables

--- semana.R
+++ semana.R
@@ -1,22 +1,16 @@
 
 # browseURL("https://nrennie.rbind.io/blog/script-templates-r/")
 
-# paquetes ----------------------------------------------------------------
-
-library(glue)
-library(stringr)
-library(crayon)
-
 # función que crea una nueva carpeta con un script para el procesamiento
-# de los datos de tidytuesday de la semana de interés
+# de los datos de {tidytuesday} de la semana de interés
 
 nueva_semana <- function(semana_numero, año = 2024) {
   
   # nombre de la carpeta a crear
   if (semana_numero <= 9) {
-    semana_carpeta <- glue("{año}/s0{semana_numero}")
+    semana_carpeta <- glue::glue("{año}/s0{semana_numero}")
   } else {
-    semana_carpeta <- glue("{año}/s{semana_numero}")
+    semana_carpeta <- glue::glue("{año}/s{semana_numero}")
   }
   
   # archivo .R
@@ -24,15 +18,15 @@ nueva_semana <- function(semana_numero, año = 2024) {
   
   # verifico que la carpeta de la semana no exista
   semanas_ok <- list.files(glue("{año}")) |> 
-    str_remove("s") |> 
+    stringr::str_remove("s") |> 
     as.numeric()
   
   if (mean(semanas_ok == semana_numero) != 0) {
     
     
-    cat(red("\n\n\nSemana ya creada\n\n\n"))
+    cat(crayon::red("\n\n\nSemana ya creada\n\n\n"))
     
-    system(glue("open {new_file}"))
+    system(glue::glue("open {new_file}"))
     
     stop()
   }
@@ -40,13 +34,23 @@ nueva_semana <- function(semana_numero, año = 2024) {
   # verifico que el número de semana sea correcto
   if (semana_numero %% as.integer(semana_numero) != 0) {
     
-    stop(glue(red("\n\n\nNúmero de semana en formato incorrecto\n\n\n")))
+    stop(
+      crayon::glue(
+        crayon::red(
+          "\n\n\nNúmero de semana en formato incorrecto\n\n\n")
+      )
+    )
   }
   
   # creo directorio
   dir.create(semana_carpeta, recursive = TRUE)
   
-  glue(blue(bold("\n\nNueva carpeta creada\n\n")))
+  glue::glue(
+    crayon::blue(
+      crayon::bold(
+        "\n\nNueva carpeta creada\n\n")
+    )
+  )
   
   if (!file.exists(new_file)) {
     file.create(new_file)
@@ -77,22 +81,28 @@ nueva_semana <- function(semana_numero, año = 2024) {
     writeLines(r_txt, con = new_file)
     
     cat(
-      blue(
-        bold(
-          glue("\n\nArchivo R creado para semana {semana_numero}\n\n"))))
+      crayon::blue(
+        crayon::bold(
+          glue::glue(
+            "\n\nArchivo R creado para semana {semana_numero}\n\n")
+        )
+      )
+    )
     
   }
   
-  system(glue("open {new_file}"))
+  system(glue::glue("open {new_file}"))
   
   source(new_file)
     
 }
 
-cat(bgRed(
-  white(
-    '\n\nUsar función ' %+%
-      bold('nueva_semana()') %+%
-      ' para iniciar el procesamiento.\n\n'
+cat(
+  crayon::bgRed(
+    crayon::white(
+      glue::glue(
+        "\n\nUsar función {crayon::bold('nueva_semana()')} ",
+        "para iniciar el procesamiento.\n\n\n")
+    )
   )
-))
+)
