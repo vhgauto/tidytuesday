326f44d2fd3a8b028fc89071b9d7aa653583efdf
Víctor Gauto <victor.gauto@outlook.com>
2024-10-17 23:22:58 -0300

    script semana 42, avistamiento de orcas

--- 2024/s42/script.R
+++ 2024/s42/script.R
@@ -4,15 +4,19 @@
 library(glue)
 library(ggtext)
 library(showtext)
+library(terra)
 library(tidyterra)
 library(tidyverse)
 
 # fuente ------------------------------------------------------------------
 
 # colores
-c1 <- "#000000"
-c2 <- "#000000"
-c3 <- "#000000"
+c1 <- "#3961FF"
+c2 <- "#9D6227"
+c3 <- "darkred"
+c4 <- "#E4E4E4"
+c5 <- "black"
+c6 <- "grey30"
 
 # fuente: Ubuntu
 font_add(
@@ -33,16 +37,16 @@ showtext_opts(dpi = 300)
 
 # caption
 fuente <- glue(
-  "Datos: <span style='color:{c3};'><span style='font-family:jet;'>",
+  "Datos: <span style='color:{c2};'><span style='font-family:jet;'>",
   "{{<b>tidytuesdayR</b>}}</span> semana {42}, ",
-  "XXX fuente de datos XXX.</span>"
+  "<b>Center for Whale Research</b>.</span>"
 )
-autor <- glue("<span style='color:{c3};'>**Víctor Gauto**</span>")
+autor <- glue("<span style='color:{c2};'>**Víctor Gauto**</span>")
 icon_twitter <- glue("<span style='font-family:jet;'>&#xf099;</span>")
 icon_instagram <- glue("<span style='font-family:jet;'>&#xf16d;</span>")
 icon_github <- glue("<span style='font-family:jet;'>&#xf09b;</span>")
 icon_mastodon <- glue("<span style='font-family:jet;'>&#xf0ad1;</span>")
-usuario <- glue("<span style='color:{c3};'>**vhgauto**</span>")
+usuario <- glue("<span style='color:{c2};'>**vhgauto**</span>")
 sep <- glue("**|**")
 
 mi_caption <- glue(
@@ -55,97 +59,102 @@ mi_caption <- glue(
 tuesdata <- tidytuesdayR::tt_load(2024, 42)
 orcas <- tuesdata$orcas
 
-glimpse(orcas)
+# me interesa los sitios de avistamiento de orcas sobre el relieve de la región
 
-count(orcas, pods_or_ecotype, sort = TRUE)
-
-library(terra)
-
-begin_coord <- vect(
+# convierto datos a vector
+orcas_sf <- vect(
   orcas, geom = c("begin_longitude", "begin_latitude"), crs = "EPSG:4326"
 )
 
-end_coord <- vect(
-  orcas, geom = c("end_longitude", "end_latitude"), crs = "EPSG:4326"
-)
+# región de interés
+usa_sf <- rgeoboundaries::gb_adm0(country = "USA")
+cnd_sf <- rgeoboundaries::gb_adm0(country = "Canada")
+aoi_sf <- rbind(usa_sf, cnd_sf) |> 
+  vect()
 
-plot(begin_coord, col = alpha("red", .6))
-points(end_coord, col = alpha("blue", .6))
+# obtengo el relieve de la región de interés
+bb <- (ext(orcas_sf)*1.2) |> 
+  vect(crs = "EPSG:4326")
 
-count(drop_na(orcas, year), year, sort = TRUE)
+dem <- elevatr::get_elev_raster(
+  locations = sf::st_as_sf(bb),
+  z = 11,
+  clip = "bbox"
+) |> 
+  rast()
 
-begin_coord$año <- factor(year(begin_coord$date))
-
-cnd_sf <- rgeoboundaries::gb_adm0(country = "Canada") |> 
-  vect() |> 
-  sf::st_as_sf()
-
-usa_sf <- rgeoboundaries::gb_adm0(country = "USA") |> 
-  vect() |> 
-  sf::st_as_sf()
-
-costa <- sf::st_union(cnd_sf, usa_sf)
-
-bb_sf <- (ext(begin_coord)*1.25) |> 
-  vect(crs = "EPSG:4326") |> 
-  sf::st_as_sf()
-
-aoi_sf <- sf::st_intersection(bb_sf, costa) |> 
-  sf::st_geometry()
-
-
-# plot(as.lines(vect(bb_sf)), col = "red")
-plot(vect(aoi_sf), axes = FALSE)
-points(begin_coord, cex = .5, col = alpha("blue", .5))
-
-cnd_crop <- crop(cnd, bb)
-
-library(sf)
-
-costa_sf <- rgeoboundaries::gb_adm0(country = c("USA", "Canada")) |> 
-  st_geometry()
-
-orcas_sf <- st_as_sf(begin_coord) |> 
-  st_geometry()
-
-# orcas_bb_sf <- st_bbox(orcas_sf) |> 
-#   st_as_sfc() |> 
-#   vect() |> 
-#   ext()
-
-# orcas_bb_sf <- vect(orcas_bb_sf*2, crs = "EPSG:4326") |> 
-#   st_as_sf()
-
-bb <- st_bbox(orcas_sf) |> 
-  st_as_sfc()
-
-costa_crop_sf <- st_intersection(costa_sf, bb)
-
-ggplot() +
-  geom_spatvector(
-    data = aoi_sf, color = "darkblue", fill = "grey70"
-  ) +
-  geom_spatvector(
-    data = orcas_sf, color = "darkred", size = 1, alpha = .6
-  ) +
-  theme_void(base_size = 3)
-
-
-
-
+# guardo ráster
+writeRaster(dem, "2024/s42/dem_11.tif", overwrite = TRUE)
+dem <- rast("2024/s42/dem_11.tif")
 
 # figura ------------------------------------------------------------------
 
+# subtítulo
+mi_subtitulo <- glue(
+  "Posiciones de avistamiento de <b style='color: {c3};'>orcas</b> en el ",
+  "<b style='color: {c1}; '>Mar de los Salish</b>.<br>Entre 2017 a 2024 se ",
+  "contabilizaron <b>{nrow(orcas_sf)}</b> registros."
+)
+
+# coordenadas
+lon <- -125:-123
+lon_label <- glue("{lon}°")
+
+lat <- 48:50
+lat_label <- glue("+{lat}°")
+
 # figura
-# g <- ggplot()
+g <- ggplot() +
+  geom_spatraster(
+    data = dem, show.legend = FALSE, maxcell = size(dem)
+  ) +
+  geom_spatvector(
+    data = orcas_sf, color = c3, alpha = .4, size = 3
+  ) +
+  scale_fill_hypso_c(palette = "gmt_globe") +
+  scale_x_continuous(
+    breaks = lon,
+    labels = lon_label
+  ) +
+  scale_y_continuous(
+    breaks = lat,
+    labels = lat_label
+  ) +
+  coord_sf(expand = FALSE) +
+  labs(subtitle = mi_subtitulo, caption = mi_caption) +
+  theme_void() +
+  theme(
+    plot.background = element_rect(
+      fill = c4, color = c1, linewidth = 3
+    ),
+    plot.margin = margin(t = 17, r = 20, b = 15, l = 20),
+    plot.subtitle = element_markdown(
+      family = "ubuntu", size = 20, hjust = .5, margin = margin(b = 10)
+    ),
+    plot.caption = element_markdown(
+      family = "ubuntu", color = c1, size = 15, lineheight = unit(1.2, "line"),
+      margin = margin(b = 5, r = 0, t = 15)
+    ),
+    panel.background = element_rect(fill = NA, color = c5, linewidth = 1),
+    panel.grid.major = element_line(
+      color = c6, linetype = "FF", linewidth = .2
+    ),
+    panel.ontop = TRUE,
+    axis.text = element_text(
+      family = "jet", margin = margin(t = 5, r = 5), size = 15
+    ),
+    axis.ticks.length = unit(2, "mm"),
+    axis.ticks = element_line(color = c5, linewidth = .7)
+  )
 
 # guardo
-# ggsave(
-#   plot = g,
-#   filename = "2024/s42/viz.png",
-#   width = 30,
-#   height = 30,
-#   units = "cm")
+ggsave(
+  plot = g,
+  filename = "2024/s42/viz.png",
+  width = 30,
+  height = 34,
+  units = "cm"
+)
 
 # abro
-# browseURL(glue("{getwd()}/2024/s42/viz.png"))
+browseURL(glue("{getwd()}/2024/s42/viz.png"))
