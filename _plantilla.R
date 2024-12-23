1aae701cede3857f8218d23ab5b3265c3cdb02bb
Víctor Gauto <victor.gauto@outlook.com>
2024-12-23 15:07:33 -0300

    remuevo caption explícito, dando lugar a la función caption()

--- _plantilla.R
+++ _plantilla.R
@@ -29,28 +29,12 @@ showtext_auto()
 showtext_opts(dpi = 300)
 
 # caption
-fuente <- glue(
-  "Datos: <span style='color:{c3};'><span style='font-family:jet;'>",
-  "{{<b>tidytuesdayR</b>}}</span> semana {semana_numero}, ",
-  "<b>FUENTE DE DATOS</b>.</span>"
-)
-autor <- glue("<span style='color:{c3};'>**Víctor Gauto**</span>")
-icon_twitter <- glue("<span style='font-family:jet;'>&#xf099;</span>")
-icon_instagram <- glue("<span style='font-family:jet;'>&#xf16d;</span>")
-icon_github <- glue("<span style='font-family:jet;'>&#xf09b;</span>")
-icon_mastodon <- glue("<span style='font-family:jet;'>&#xf0ad1;</span>")
-icon_bsky <- glue("<span style='font-family:jet;'>&#xe28e;</span>")
-usuario <- glue("<span style='color:{c3};'>**vhgauto**</span>")
-sep <- glue("**|**")
-
-mi_caption <- glue(
-  "{fuente}<br>{autor} {sep} {icon_github} {icon_twitter} {icon_instagram} ",
-  "{icon_mastodon} {icon_bsky} {usuario}"
-)
+mi_caption <- caption(fuente1 = "FUENTE DE DATOS", col = c1)
 
 # datos -------------------------------------------------------------------
 
 tuesdata <- tidytuesdayR::tt_load(año, semana_numero)
+tuesdata
 
 # figura ------------------------------------------------------------------
 
